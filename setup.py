d15e6a7c28379dff807b2774c777fe91fa6203e2
Albert Swart <adswart@gmail.com>
2022-03-22 11:00:11 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022
+# Author: Niko Brummer
+# All Rights Reserved
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='PSDA',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/bsxfan/PSDA',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    license='',
+    author='Niko Brummer',
+    author_email='niko.brummer@gmail.com',
+    description='Python implementation of Probabilistic Spherical Discriminant Analysis.'
+)
