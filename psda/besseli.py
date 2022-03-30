df0ef23fc08b6c9ee9383a4c4b9cfbec46f6c930
bsxfan <bsxfan@gmail.com>
2022-03-30 17:54:49 +0200

    Update besseli.py

--- psda/besseli.py
+++ psda/besseli.py
@@ -20,13 +20,35 @@ def logBesselI_ive(nu, x):
 
 
 class LogBesselI:
+    """
+    Callable to implement log I_nu(x), for nu >=0 and x >= 0.
+    
+    Unlike scipy.special.ive, this callable will not underflow if x is small
+    relative to nu.
+    
+    The degree, nu is stored in the callable, while x is supplied to the call,
+    e.g.:
+    
+        logI =  LogBesselI(nu)
+        y1 = logI(x1)
+        y2 = logI(x2)
+    
+    I_nu(x) >= 0, so log is -inf or real 
+    I_nu(x) (and its log) is monotonic rising
+    log I_0(0) = 0, 
+    log I_nu(0) = -inf (no warning), for nu > 0
+    
+    """
+    
+    
     def __init__(self, nu, n=5):
+        assert nu >= 0, 'Bessel-I is defined for nu < 0, but this code excludes that.'
         self.nu = nu
         self.n = n
         m = np.arange(n)
         self.exp = (2*m+nu).reshape(-1,1)
         self.den = (logfactorial(m) + gammaln(m+1+nu)).reshape(-1,1)
-        self.thr = np.sqrt(self.nu+1)
+        self.at0 = 0.0 if nu==0 else -np.inf
 
     def small(self,logx):
         """
@@ -36,65 +58,113 @@ class LogBesselI:
         return logsumexp(num-self.den,axis=0)
 
 
-    def large(self, x):
-        
-        return 
 
-
-
-    def __call__(self, x):
+    def __call__(self, x, logx = None):
+        """
+        Also supply logx if available
+        """
         if np.isscalar(x):
+            assert x >= 0
+            if x == 0: return self.at0
             return self.__call__(np.array([x]))[0]
-        y = ive(self.nu,x)
-        nz = y > 0
-        zz = np.logical_not(nz)
-        y[nz] = np.log(y[nz]) + x[nz]
-        y[zz] = self.small(np.log(x[zz]))
+        assert all(x >= 0)
+        y = ive(self.nu,x)  
+        ok = np.logical_or(x==0, y > 0) # ive gives correct answer (0 or 1) for x==0
+        uf = np.logical_not(ok)                # underflow if y==0 and x > 0
+        with np.errstate(divide='ignore'):
+            y[ok] = np.log(y[ok]) + x[ok]      # y may be 0 if x ==0 
+        logx =  np.log(x[uf]) if logx is None else logx[uf]
+        y[uf] = self.small(logx)      # fix underflows 
         return y
 
 
 
-# Doesn't work and is slower than ive in any case
-# class LogBesselI_big:
-#     def __init__(self, nu, n=5):
-#         """
-#         Won't work for (2n-3)**2 >= 4nu**2
-#         For now it's OK, because we will have large nu and small n'
+class LogBesselIPair:
+    """
+    This is a callable that computes log I_nu and log I_{nu+1} and their
+    derivatives in a single call.
+    
+    The degree nu is fixed within the object.
+    
+    To compute rho = I_{n+1} / I_nu, you need both I's and then the derivatives 
+    are almost free:
         
-#         To make it work more generally, change the code to process the sign of 
-#         delta below separately.
+    d/dx I(nu,x) = I(nu-1, z) - (nu/x)I(nu,x)
+                 = (nu/x)I(nu,x) + I(nu+1,x)
+                 = (I(nu-1,x) + I(nu+1,x)) / 2
         
-#         """
-#         self.nu = nu
-#         self.n = n
-#         logw = np.zeros(n)
-#         nu2 = nu**2
-#         for i in range(1,n):
-#             odd = 2*i-1               # 1, 3, 5, ...
-#             delta = 4*nu2-odd**2
-#             assert delta > 0, "n too large wrt nu (can be fixed by some coding here)"
-#             logw[i] = logw[i-1] + np.log(delta) \
-#                                 - np.log(8*i)
-#         self.logwpos = logw[0:n:2].reshape(-1,1)                        
-#         self.logwneg = logw[1:n:2].reshape(-1,1)
-#         exp = np.arange(n)
-#         self.exppos = exp[0:n:2].reshape(-1,1)
-#         self.expneg = exp[1:n:2].reshape(-1,1)
-                
+             so
+    
+    d/dx I(nu+1,x) = I(nu, z) - ((nu+1)/x) I(nu+1,x)
+    
+    
+    
+    """
+    def __init__(self, nu, n = 5):
+        self.nu = nu
+        self.logI = LogBesselI(nu,n)
+        self.logI1 = LogBesselI(nu+1,n)
+        
+    def __call__(self, logx):
+        """
+        For y, y1 = logI_nu(x), logI_{nu+1}, 
+        returns: y, y1, dy_dlogx, dy1_dlogx
+        
+        
+        """
+        nu = self.nu
+        x = np.exp(logx)
+        y = self.logI(x,logx)
+        y1 = self.logI1(x,logx)
+        dy_dlogx = nu + np.exp(logx + y1 - y)    
+        dy1_dlogx = np.exp(logx + y - y1) - (nu+1)
+        return y, y1, dy_dlogx, dy1_dlogx
+        
+
+    def logI(self, logx):
+        """
+        For y = logI_nu(x)
+        returns: y, dy_dlogx
+        
+        
+        """
+        nu = self.nu
+        x = np.exp(logx)
+        y = self.logI(x,logx)
+        y1 = self.logI1(x,logx)
+        dy_dlogx = nu + np.exp(logx + y1 - y)    
+        return y, dy_dlogx
 
 
-#     def __call__(self,logx):
-#         logwpos, logwneg = self.logwpos, self.logwneg
-#         exppos, expneg = self.exppos, self.expneg
+
         
-#         pos = logsumexp(logx*exppos + logwpos, axis=0) 
-#         neg = logsumexp(logx*expneg + logwneg, axis=0)
-#         delta = neg - pos
-#         #assert all(delta < 0)
-#         delta = np.abs(delta)           
-#         f = np.log1p(np.exp(delta))
-#         x = np.exp(logx)
-#         return x + f - (logx-log2pi)/2
+    def logRho(self,logx):
+        """
+        rho(x) = I_nu+1(x) / I_nu(x), for x > 0
+        
+        return: log rho(x), dlogrho_dlox
+    
+        """
+
+        if np.isscalar(logx):
+            y, dy_dlogx = self.logRho(np.array([logx]))
+            return y[0], dy_dlogx[0]
+
+
+        y, y1, dy_dlogx, dy1_dlogx = self(logx)
+        return y1-y, dy1_dlogx - dy_dlogx
+    
+    
+    def rho(self,x):
+        logx = np.log(x)
+        logr, dlogr_dlogx = self.logRho(logx)
+        r = np.exp(logr)
+        dr_dx = r * dlogr_dlogx / x
+        return r, dr_dx
+
+
+
+
     
 if __name__ == "__main__":
     
@@ -111,7 +181,9 @@ if __name__ == "__main__":
     small = logBesselI.small(logk)
     splice = logBesselI(k)
     
-    ref = logBesselI_ive(nu, k)
+    
+    with np.errstate(divide='ignore'):
+        ref = logBesselI_ive(nu, k)
     
     
     
@@ -122,6 +194,19 @@ if __name__ == "__main__":
     plt.grid()
     plt.show()
     
+    
+    
+    
+    pair = LogBesselIPair(100)
+    logx = np.linspace(-6,14,200)
+    logr, ddlogx = pair.logRho(logx)
+    plt.plot(logx,np.exp(logr),label='rho')
+    plt.plot(logx,ddlogx*np.exp(logr),label='dy/dx')
+    plt.grid()
+    plt.xlabel('log k')
+    plt.ylabel('rho')
+    plt.title('nu = 100')
+    plt.legend()
     
     
     
