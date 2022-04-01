2bd9e73cbaafcb73bd158d97287a38020f401182
bsxfan <bsxfan@gmail.com>
2022-04-01 19:32:36 +0200

    Update fastvmf.py

--- psda/fastvmf.py
+++ psda/fastvmf.py
@@ -11,10 +11,6 @@ class FastLogI:
         self.slow = fast.slow                 # slower reference function
         self.abcd = abcd = fast.params
         
-        d = abcd[-1]
-        self.fast1 = fast1 = fastLogCvmf_e(nu+1,d=d,tune=False)
-        #self.fast1 = fast1 = fastLogCvmf_e(nu+1,d=d)
-        self.slow1 = fast1.slow
         
         
     def logCvmf_e(self, log_kappa, slow = False):
@@ -32,14 +28,41 @@ class FastLogI:
         return nu*log_kappa - self.logCvmf_e(log_kappa, slow)
     
     
-    def logrho(self, log_kappa, slow = False):
-        if slow:
-            return log_kappa + self.slow(log_kappa) - self.slow1(log_kappa) 
-        else:
-            return log_kappa + self.fast(log_kappa) - self.fast1(log_kappa) 
+    # def logrho(self, log_kappa, slow = False):
+    #     if slow:
+    #         return log_kappa + self.slow(log_kappa) - self.slow1(log_kappa) 
+    #     else:
+    #         return log_kappa + self.fast(log_kappa) - self.fast1(log_kappa) 
             
-    def rho(self, log_kappa, slow = False):
-        return np.exp(self.logrho(log_kappa, slow))
+    # def rho(self, log_kappa, slow = False):
+    #     return np.exp(self.logrho(log_kappa, slow))
+    
+    
+    
+class Sra:
+    def __init__(self,nu):
+        self.nu = nu
+        self.dim = 2*(nu+1)
+    
+    def invrho(self, r):
+        dim = self.dim
+        rr = r**2
+        return np.log(r*(dim-rr)/(1-rr))
+    
+    
+    # doesn't work
+    # def rho(self, logk):
+    #     if not np.isscalar(logk):
+    #         return np.array([self.rho(logki) for logki in logk])
+    #     dim = self.dim
+    #     k = np.exp(logk)
+    #     r = 0.5
+    #     for i in range(10):
+    #         rr = r**2
+    #         r = k*(1-rr)/(dim-rr)
+    #     return r    
+            
+    
 
 
 
@@ -48,7 +71,7 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     
     
-    dim = 128
+    dim = 5
     
     
     nu = dim/2 -1
@@ -77,14 +100,20 @@ if __name__ == "__main__":
     
     log_kappa = np.linspace(-5,15,200)
     
-    # fast = np.exp(logrho(log_kappa))
-    # slow = np.exp(logrho.slow(log_kappa))
-    fast = logrho(log_kappa)
-    slow = logrho.slow(log_kappa)
+    fast = np.exp(logrho(log_kappa))
+    slow = np.exp(logrho.slow(log_kappa))
+    #fast = logrho(log_kappa)
+    #slow = logrho.slow(log_kappa)
+    
+    sra = lambda r: r*(dim-r**2)/(1-r**2)
+    rr = np.linspace(0.001,0.999,200)
+    
     
     plt.figure()
     plt.plot(log_kappa,slow,label='slow')
-    plt.plot(log_kappa,fast,'--',label='fast')
+    #plt.plot(log_kappa,fast,'--',label='fast')
+    # plt.plot(np.log(sra(rr)),np.log(rr),'--',label='sra')
+    plt.plot(np.log(sra(rr)),rr,'--',label='sra')
     plt.grid()
     plt.legend()
     plt.xlabel('log kappa')
